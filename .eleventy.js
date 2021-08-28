7135841e82322b1df550c61ba528e511167e4514
Paul Applegate <paul@theapplegates.com>
2021-08-28 23:35:14 +0000

    added cloudinary image

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,56 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const Image = require("@11ty/eleventy-img");
+const path = require('path');
+
+async function imageShortcode(src, alt) {
+  let sizes = "(min-width: 1024px) 100vw, 50vw"
+  let srcPrefix = `./img/`
+  src = srcPrefix + src
+  console.log(`Generating image(s) from:  ${src}`)
+  if(alt === undefined) {
+    // Throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+  }
+  let metadata = await Image(src, {
+    widths: [600, 900, 1500],
+    formats: ['avif', 'webp', 'jpg'],
+    urlPath: "/img/",
+    outputDir: "./_site/img/",
+    /* =====
+    Now we'll make sure each resulting file's name will
+    make sense to you. **This** is why you need
+    that `path` statement mentioned earlier.
+    ===== */
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
+      return `${name}-${width}w.${format}`
+    }
+  })
+  let lowsrc = metadata.jpg[0]
+  let highsrc = metadata.jpg[metadata.jpg.length - 1]
+  return `<picture>
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+    }).join("\n")}
+    <img
+      src="${lowsrc.url}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
+      alt="${alt}"
+      loading="lazy"
+      decoding="async">
+  </picture>`
+}
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+  eleventyConfig.addLiquidShortcode("image", imageShortcode)
+  // === Liquid needed if `markdownTemplateEngine` **isn't** changed from Eleventy default
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode)
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
--- posts/fourthpost.md
+++ posts/fourthpost.md
@@ -5,6 +5,12 @@ date: 2018-09-30
 tags: second tag
 layout: layouts/post.njk
 ---
+
+<img data-src="https://res.cloudinary.com/paulportfolio/image/upload/f_auto,q_auto,w_auto,c_scale/v1594406263/blog/IMG_1291.jpg" alt="" class="cld-responsive">
+
+
+![Africa](https://res.cloudinary.com/paulportfolio/image/upload/f_auto,q_auto/ar_4:3,c_fill/c_scale,w_auto/dpr_auto/v1630074929/11ty/African_mosaic.jpg)
+
 Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
 
 Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
